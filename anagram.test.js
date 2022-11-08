06a4cb5ce2b1da250bef2e9684b51ccd2010f7f3
serenakang <serenakang77@gmail.com>
2022-11-08 01:49:08 -0500

    Added testwatch and some function

--- anagram.test.js
+++ anagram.test.js
@@ -1,17 +1,17 @@
-const isAnagram = require("./anagram")
+const isAnagram = require("./anagram");
 
 test("isAnagram function exists", () => {
-  expect(typeof isAnagram).toEqual("function")
-})
+  expect(typeof isAnagram).toEqual("function");
+});
 
 test(`"cinema" is an anagram of "iceman"`, () => {
-  expect(isAnagram("cinema", "iceman")).toBeTruthy()
-})
+  expect(isAnagram("cinema", "iceman")).toBeTruthy();
+});
 
 test(`"Dormitory" is an anagram of "dirty room##"`, () => {
-  expect(isAnagram("Dormitory", "dirty room##")).toBeTruthy()
-})
+  expect(isAnagram("Dormitory", "dirty room##")).toBeTruthy();
+});
 
 test(`"Hello" is NOT an anagram of "dirty Aloha"`, () => {
-  expect(isAnagram("Hello", "Aloha")).toBeFalsy()
-})
+  expect(isAnagram("Hello", "Aloha")).toBeFalsy();
+});
--- functions.test.js
+++ functions.test.js
@@ -1,66 +1,76 @@
+
 const functions = require("./functions")
 
-const nameCheck = () => console.log("Checking Name....")
+// beforeEach(() => initDatabase());
+// afterEach(() => closeDatabase());
+
+// beforeAll(() => initDatabase());
+// afterAll(() => closeDatabase());
+
+// const initDatabase = () => console.log("Database Initialized");
+// const closeDatabase = () => console.log("Database Closed");
+
+const nameCheck = () => console.log("Checking Name...");
 
 describe("Checking Names", () => {
-  beforeEach(() => nameCheck())
+    beforeEach(() => nameCheck());
 
-  test("User is Jeff", () => {
-    const user = "Jeff"
-    expect(user).toBe("Jeff")
-  })
+    test("User is Jeff", () => {
+        const user = "Jeff";
+        expect(user).toBe("Jeff");
+    });
 
-  test("User is Karen", () => {
-    const user = "Karen"
-    expect(user).toBe("Karen")
-  })
-})
+    test("User is Serena", () => {
+      const user = "Serena"
+      expect(user).toBe("Serena")
+    });
+});
 
 // toBe
 test("Adds 2 + 2 to equal 4", () => {
-  expect(functions.add(2, 2)).toBe(4)
-})
+  expect(functions.add(2, 2)).toBe(4);
+});
 
 // not
 test("Adds 2 + 2 to NOT equal 5", () => {
-  expect(functions.add(2, 2)).not.toBe(5)
-})
+  expect(functions.add(2, 2)).not.toBe(5);
+});
 
 // toBeNull
 test("Should be null", () => {
-  expect(functions.isNull()).toBeNull()
-})
+  expect(functions.isNull()).toBeNull();
+});
 
 // toBeFalsy
 test("Should be falsy", () => {
-  expect(functions.checkValue(undefined)).toBeFalsy()
-})
+  expect(functions.checkValue(undefined)).toBeFalsy();
+});
 
 // toEqual
 test("User should be Brad Traversy object", () => {
   expect(functions.createUser()).toEqual({
     firstName: "Serena",
     lastName: "Kang",
-  })
+  });
 })
 
 // Less than and greater than
 test("Should be under 1600", () => {
   const load1 = 800
   const load2 = 800
-  expect(load1 + load2).toBeLessThanOrEqual(1600)
-})
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
 
 // Regex
 test("There is no I in team", () => {
-  expect("team").not.toMatch(/I/i)
-})
+  expect("team").not.toMatch(/I/i);
+});
 
 // Arrays
 test("Admin should be in usernames", () => {
   usernames = ["john", "karen", "admin"]
-  expect(usernames).toContain("admin")
-})
+  expect(usernames).toContain("admin");
+});
 
 // Working with async data
 
@@ -76,5 +86,5 @@ test("Admin should be in usernames", () => {
 test("User fetched name should be Leanne Graham", async () => {
   expect.assertions(1)
   const data = await functions.fetchUser()
-  expect(data.name).toEqual("Leanne Graham")
-})
+  expect(data.name).toEqual("Leanne Graham");
+});
